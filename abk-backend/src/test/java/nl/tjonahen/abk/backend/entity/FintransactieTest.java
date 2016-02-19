81a780251e60c0b1baa8a15d5a1f0e02e43944d0
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2016-02-19 19:15:29 +0100

    Fixed some little sonar issues

---
+++ abk-backend/src/test/java/nl/tjonahen/abk/backend/entity/FintransactieTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2016 Philippe Tjon - A - Hen, philippe@tjonahen.nl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.tjonahen.abk.backend.entity;
+
+import java.util.Date;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Philippe Tjon - A - Hen, philippe@tjonahen.nl
+ */
+public class FintransactieTest {
+
+    /**
+     * Test of hashCode method, of class Fintransactie.
+     */
+    @Test
+    public void testHashCode() {
+        Fintransactie instance = new Fintransactie();
+        int expResult = 0;
+        int result = instance.hashCode();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of equals method, of class Fintransactie.
+     */
+    @Test
+    public void testEquals() {
+        Fintransactie instance = new Fintransactie();
+        assertFalse(instance.equals("a string"));
+        Fintransactie other = new Fintransactie();
+        assertTrue(instance.equals(other));
+        other.setId(10L);
+        assertFalse(instance.equals(other));
+        assertFalse(other.equals(instance));
+
+        instance.setId(10L);
+        assertTrue(instance.equals(other));
+
+    }
+
+    /**
+     * Test of toString method, of class Fintransactie.
+     */
+    @Test
+    public void testToString() {
+        Fintransactie instance = new Fintransactie();
+        String expResult = "nl.tjonahen.abk.backend.entity.Fintransactie[ id=null ]";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+    }
+
+}
