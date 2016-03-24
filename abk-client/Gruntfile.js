5d4ff44d2b248cf3354c329b874d60c0fd737bfa
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2016-03-24 15:49:08 +0100

    added maven to frondend build

--- abk-client/Gruntfile.js
+++ abk-client/Gruntfile.js
@@ -424,8 +424,8 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('default', [
-        'newer:jshint',
-        'test',
-        'build'
+//        'test',
+        'build',
+        'sonarRunner'
     ]);
 };
